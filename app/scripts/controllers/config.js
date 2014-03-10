a62971cf7fd21fe371c6fc91cb41486ea2a347ad
Robert Danitz <robert.danitz@fokus.fraunhofer.de>
2014-03-10 14:36:12 +0100

    done datasource config

--- app/scripts/controllers/config.js
+++ app/scripts/controllers/config.js
@@ -1,25 +1,24 @@
 R2rDesigner.ConfigController = Ember.ObjectController.extend({
   needs: ['config', 'datasources', 'mappings', 'vocabs'],
 
-  new_ds : null,
-
   actions : {
     newDatasource : function () {
-      this.new_ds = this.store.createRecord("Datasource", {
+      this.set('new_ds', this.store.createRecord("Datasource", {
         name : "",
         connection_uri : "",
         classname : "",
         subprotocol : "",
         username : "",
         password : ""
-      });
+      }));
     },
 
-    saveDatasource : function (new_ds) {
-      console.log(new_ds);
-      console.log(this.get('new_ds'));
-      this.get('new_ds').save();
-      this.set('current_ds', this.get('new_ds'));
+    saveDatasource : function (ds) {
+      ds.save();
+    },
+
+    cancelNewDatasource : function (ds) {
+      ds.rollback();
     }
   }
 });
