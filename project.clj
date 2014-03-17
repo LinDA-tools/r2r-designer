a33546d9739acd8490e465e6c25392784050c25a
Robert Danitz <robert.danitz@fokus.fraunhofer.de>
2014-03-17 16:45:12 +0100

    aligns to app/server dir layout

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject r2r-server "0.1.0-SNAPSHOT"
-  :description "TODO"
-  :url "TODO"
-  :license {:name "TODO: Choose a license"
+  :description "r2r-designer server component"
+  :url "http://linda-project.eu"
+  :license {:name "MIT"
             :url "http://choosealicense.com/"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
-                   :source-paths ["dev"]}})
+                   :source-paths ["server/dev" "server/src"]}})
