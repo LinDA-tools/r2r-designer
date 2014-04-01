14ec7fb356c5a1c42b251dfe9c5e5540643f8eec
Robert Danitz <robert.danitz@fokus.fraunhofer.de>
2014-04-01 19:57:33 +0200

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular
+  .module('r2rDesignerApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
