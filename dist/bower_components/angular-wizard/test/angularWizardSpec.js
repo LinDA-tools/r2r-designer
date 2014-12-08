5f4d59d8d6c058641305946ce47ece0b6a1cec04
Lena Farid <lena.farid@fokus.fraunhofer.de>
2014-12-08 06:29:15 -0800

    linda workbench specifics

--- dist/bower_components/angular-wizard/test/angularWizardSpec.js
+++
@@ -1,7 +0,0 @@
-describe( 'AngularWizard', function() {
-  beforeEach( module( 'mgo-angular-wizard' ) );
-
-  it( 'should have a dummy test', inject( function() {
-    expect( true ).toBeTruthy();
-  }));
-});
